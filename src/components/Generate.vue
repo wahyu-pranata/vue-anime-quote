d0ff5f526c788448222ea9968efd5b4c48764324
Wahyu Pranata <wahyupranata131@gmail.com>
2022-03-21 20:43:10 +0800

    emit event and export props from App.vue, add some styles

---
+++ src/components/Generate.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click="$emit('generated')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    padding: 4px 12px;
+    margin-top: 12px;
+    border: 2px solid #562135;
+    background-color: #74204A;
+    font-size: 1.02em;
+    color: #fff;
+    cursor: pointer;
+  } 
+  button:hover {
+    opacity: 0.9;
+  }
+  button:active {
+    opacity: 0.7;
+  }
+</style>
